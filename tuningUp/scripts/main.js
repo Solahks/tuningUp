d2605a1e66828fab0f208d973804d98c7898d2a9
Solah <148382884+Solahks@users.noreply.github.com>
2024-01-11 00:14:05 -0600

    Sound working!
    Lowe string sound added

--- tuningUp/scripts/main.js
+++ tuningUp/scripts/main.js
@@ -1,12 +1,24 @@
-const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
-const mySix = documentquery.selectorAll("stringSix")
+// const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+// const mySix = document.querySelectorAll("stringSix");
 
-mySix.forEach(stringSix => {
-  stringSix.addEventListener("click", () => {
-    audio.play();
-  });
+// mySix.forEach((stringSix) => {
+  // mySix.addEventListener("click", () => {
+  //   console.log("button got clicked")
+  //   audio.play();
+  // });
+// });
+
+// mySix.onclick = () => {
+//   console.log("the button got clicked")
+//   audio.play();
+// };
+
+// console.log(mySix);
+
+const audio = new Audio("scripts/lowe.mp3");
+console.log(audio)
+const audioButton = document.getElementById("audio-button");
+
+audioButton.addEventListener("click", () => {
+  audio.play();
 });
-
-
-console.log (mySix)
-
