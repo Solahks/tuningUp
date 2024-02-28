f508508302c9c87e2b8537a742ba588729270f1f
Solah <148382884+Solahks@users.noreply.github.com>
2024-02-27 23:11:49 -0600

    Updated JS and styling
    working js, styling for the buttons, and sounds added in for standard tuning

--- tuningUp/scripts/main.js
+++ tuningUp/scripts/main.js
@@ -1,24 +1,74 @@
-// const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
-// const mySix = document.querySelectorAll("stringSix");
+//standard E audio files
+const audioFilesStandardE = [
+  new Audio("audio/lowe.mp3"),
+  new Audio("audio/String5A.mp3"),
+  new Audio("audio/String4D.mp3"),
+  new Audio("audio/String3G.mp3"),
+  new Audio("audio/String2B.mp3"),
+  new Audio("audio/String1HE.mp3"),
+];
 
-// mySix.forEach((stringSix) => {
-  // mySix.addEventListener("click", () => {
-  //   console.log("button got clicked")
-  //   audio.play();
-  // });
+//connect buttons
+const mySix = document.querySelectorAll(".string");
+
+//function to loop audio files with buttons
+for (let i = 0; i < mySix.length; i++) {
+  mySix[i].addEventListener("click", () => {
+    audioFilesStandardE[i].play();
+  })
+}
+//To Do
+//Define pic files for each Audio file
+//Write a function that If this audio file plays, then show this pic file (corrosponding notes)
+
+
+
+
+
+
+
+
+
+
+//*************************************************************** *//
+//bad way- but buttons, sounds work
+// const audio6 = new Audio("audio/lowe.mp3");
+// const audioButton6 = document.getElementById("audio-button6");
+
+// audioButton6.addEventListener("click", () => {
+//   audio6.play();
 // });
 
-// mySix.onclick = () => {
-//   console.log("the button got clicked")
-//   audio.play();
-// };
+// const audio5 = new Audio("audio/lowe.mp3");
+// const audioButton5 = document.getElementById("audio-button5");
+// audioButton5.addEventListener("click", () => {
+//   audio5.play();
+// });
 
-// console.log(mySix);
+// const audio4 = new Audio("audio/lowe.mp3");
+// const audioButton4 = document.getElementById("audio-button4");
 
-const audio = new Audio("scripts/lowe.mp3");
-console.log(audio)
-const audioButton = document.getElementById("audio-button");
+// audioButton4.addEventListener("click", () => {
+//   audio4.play();
+// });
 
-audioButton.addEventListener("click", () => {
-  audio.play();
-});
+// const audio3 = new Audio("audio/lowe.mp3");
+// const audioButton3 = document.getElementById("audio-button3");
+
+// audioButton3.addEventListener("click", () => {
+//   audio3.play();
+// });
+
+// const audio2 = new Audio("audio/lowe.mp3");
+// const audioButton2 = document.getElementById("audio-button2");
+
+// audioButton2.addEventListener("click", () => {
+//   audio2.play();
+// });
+
+// const audio1 = new Audio("audio/lowe.mp3");
+// const audioButton1 = document.getElementById("audio-button1");
+
+// audioButton1.addEventListener("click", () => {
+//   audio1.play();
+// });
