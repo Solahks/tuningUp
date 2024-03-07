8238a76963243bb0b77db6570c5299b9ce471a6d
Solah <148382884+Solahks@users.noreply.github.com>
2024-03-06 22:48:35 -0600

    Menu working, CSS and HTML cleaned up
    Drop down menu working, switches audios... first two options set up.  Cleaned up HTML and CSS

--- tuningUp/scripts/main.js
+++ tuningUp/scripts/main.js
@@ -8,79 +8,50 @@ const audioFilesStan = [
 ];
 
 const audioFilesOpenA = [
-  new Audio("audio/lowe.mp3"),
-  new Audio("audio/String5A.mp3"),
-  new Audio("audio/String4D.mp3"),
-  new Audio("audio/String3G.mp3"),
-  new Audio("audio/String2B.mp3"),
-  new Audio("audio/String1HE.mp3"),
+  new Audio("audioOpenA/String6LE.mp3"),
+  new Audio("audioOpenA/String5A.mp3"),
+  new Audio("audioOpenA/String4C.mp3"),
+  new Audio("audioOpenA/String3E.mp3"),
+  new Audio("audioOpenA/String2A.mp3"),
+  new Audio("audioOpenA/String1HE.mp3"),
 ];
 
 const mySix = document.querySelectorAll(".string");
-var tuningMenu = document.querySelector(".tuningMenu");
 
-//loop audio files with buttons
+//establish state
+let menu = document.getElementById("menuSelect");
+
+//listen click on menu, update state
+menu.onchange = updateMenu;
+
+//identify what option is selected
+//set state to corresponding audio
+function updateMenu() {
+  console.log(menu.value);
+  if (menu.value === "1") { //=== or == comparative = is declarative 
+    console.log("option1selected");
+    audios = audioFilesStan;
+  }
+  if (menu.value === "2") {
+    console.log("option2selected");
+    audios = audioFilesOpenA;
+  }
+  if (menu.value === "default") {
+    audios = null;
+  }
+}
+
+//play audio- can target any audios
 for (let i = 0; i < mySix.length; i++) {
   mySix[i].addEventListener("click", () => {
-    audioFilesStan[i].play();
+    audios[i].play();
   });
 }
 
-//loop audio files with buttons and change tunings
+//original loop to just play standard strings no managed state
+//*****************************************************************
 // for (let i = 0; i < mySix.length; i++) {
 //   mySix[i].addEventListener("click", () => {
-//     if (tuningMenu === ".tuningStandard") {
-//       audioFilesStan[i].play();
-//     } else if (tuningMenu === ".tuningMajorOpenA") {
-//       console.log("hello");
-//     }
+//     audioFilesStan[i].play();
 //   });
 // }
-
-//To Do
-//menu bar to change tuning? drop down? fixed?
-//connect buttons to tuning menu
-//Pic files appear for each time an audio file plays
-
-//*************************************************************** *//
-//bad way- but buttons, sounds work
-// const audio6 = new Audio("audio/lowe.mp3");
-// const audioButton6 = document.getElementById("audio-button6");
-
-// audioButton6.addEventListener("click", () => {
-//   audio6.play();
-// });
-
-// const audio5 = new Audio("audio/lowe.mp3");
-// const audioButton5 = document.getElementById("audio-button5");
-// audioButton5.addEventListener("click", () => {
-//   audio5.play();
-// });
-
-// const audio4 = new Audio("audio/lowe.mp3");
-// const audioButton4 = document.getElementById("audio-button4");
-
-// audioButton4.addEventListener("click", () => {
-//   audio4.play();
-// });
-
-// const audio3 = new Audio("audio/lowe.mp3");
-// const audioButton3 = document.getElementById("audio-button3");
-
-// audioButton3.addEventListener("click", () => {
-//   audio3.play();
-// });
-
-// const audio2 = new Audio("audio/lowe.mp3");
-// const audioButton2 = document.getElementById("audio-button2");
-
-// audioButton2.addEventListener("click", () => {
-//   audio2.play();
-// });
-
-// const audio1 = new Audio("audio/lowe.mp3");
-// const audioButton1 = document.getElementById("audio-button1");
-
-// audioButton1.addEventListener("click", () => {
-//   audio1.play();
-// });
