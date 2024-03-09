9b00f0edd69e8504eef87991862c5c314b7ce47b
Solah <148382884+Solahks@users.noreply.github.com>
2024-03-08 23:05:05 -0600

    Title Change/css updates
    Changed title of the project; Did some major css changes and cleaned more up on the buttons

---
+++ tuneUpMy6string/scripts/main.js
@@ -0,0 +1,49 @@
+const audioFilesStan = [
+  new Audio("audio/lowe.mp3"),
+  new Audio("audio/String5A.mp3"),
+  new Audio("audio/String4D.mp3"),
+  new Audio("audio/String3G.mp3"),
+  new Audio("audio/String2B.mp3"),
+  new Audio("audio/String1HE.mp3"),
+];
+
+const audioFilesOpenA = [
+  new Audio("audioOpenA/String6LE.mp3"),
+  new Audio("audioOpenA/String5A.mp3"),
+  new Audio("audioOpenA/String4C.mp3"),
+  new Audio("audioOpenA/String3E.mp3"),
+  new Audio("audioOpenA/String2A.mp3"),
+  new Audio("audioOpenA/String1HE.mp3"),
+];
+
+const mySix = document.querySelectorAll(".string");
+
+//establish state
+let menu = document.getElementById("menuSelect");
+
+//listen click on menu, update state
+menu.onchange = updateMenu;
+
+//identify what option is selected
+//set state to corresponding audio
+function updateMenu() {
+  console.log(menu.value);
+  if (menu.value === "1") { //=== or == comparative = is declarative 
+    console.log("option1selected");
+    audios = audioFilesStan;
+  }
+  if (menu.value === "2") {
+    console.log("option2selected");
+    audios = audioFilesOpenA;
+  }
+  if (menu.value === "0") {
+    audios = null;
+  }
+}
+
+//play audio- can target any audios
+for (let i = 0; i < mySix.length; i++) {
+  mySix[i].addEventListener("click", () => {
+    audios[i].play();
+  });
+}
